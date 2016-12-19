004ba9a2c31048f0de9b66009c881d227f955e0a
Christopher Adams <christopher@fabricatorz.com>
2016-12-19 15:58:38 +0800

    Add the other photo tags and layouts for them

--- config/config-sample.rb
+++ config/config-sample.rb
@@ -1,4 +1,4 @@
 FLICKR_API_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 FLICKR_API_SECRET = "XXXXXXXXXXXXXXXX"
 
-FLICKR_QUERIES = { usernames: ["joi"], tags: ["freesoulsbook", "newrelease"] }
+FLICKR_QUERIES = { usernames: ["joi"], tags: ["freesoulsbook", "modelrelease", "newrelease", "needrelease"] }
